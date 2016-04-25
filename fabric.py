8f1b967c354ed7c02dc409c53dd35ff7027f4f63
pycole <pycole@outlook.com>
2016-04-25 14:06:02 +0800

    Create fabric.py

---
+++ fabric.py
@@ -0,0 +1,12 @@
+import os
+def fab(n):
+  if n==1:
+    return 1
+  if n==0:
+    return 0
+  else:
+    result=int(fab(n-1))+int(fab(n-2))    
+    return result
+    
+for i in range(111):     #give a test in range 0---111
+  print(fab(i))
